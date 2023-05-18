ecffa419696b670f400d957822595c3f6b4c3a52
Usman Hashmi <55950977+usmanhashmis@users.noreply.github.com>
2023-05-19 01:09:40 +0500

    Update giftoff.js
    added

--- routes/giftoff.js
+++ routes/giftoff.js
@@ -26,14 +26,14 @@ router.post("/discount", async function (req, res) {
         return res.status(400).send("Already used")
       }
       else{
-             res.send({
-                 amount: getAlldis.amount,
-                 promocode:getAlldis.promocode,
-                 coin:getAlldis.coin,
-                 alerts:"Apply successfully"
+//              res.send({
+//                  amount: getAlldis.amount,
+//                  promocode:getAlldis.promocode,
+//                  coin:getAlldis.coin,
+//                  alerts:"Apply successfully"
     
-    });
-    //    res.status(200).send("Apply successfully")
+//     });
+       res.status(200).send("Apply successfully")
       }
     }
     else
